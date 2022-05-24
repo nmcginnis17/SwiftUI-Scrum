d265ef782e135b0f64f22ad7eb75d7932c3832c5
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-05-23 22:25:27 -0400

    SwiftUI Scrum

---
+++ Project Managed/Project_ManagedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_ManagedApp.swift
+//  Project Managed
+//
+//  Created by Nicholas McGinnis on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Project_ManagedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
