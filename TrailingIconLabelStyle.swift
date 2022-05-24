6b6367b3c4961cef66eee149b72260aaa20a6e0c
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2022-05-23 22:50:16 -0400

    Added card view, label style

---
+++ TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Project Managed
+//
+//  Created by Nicholas McGinnis on 5/23/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
